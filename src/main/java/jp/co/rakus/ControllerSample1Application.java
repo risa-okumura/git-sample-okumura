98275eb020670d65d3dce5f52d68c2a5261ccfa9
okumura <risa.okumura@rakus.co.jp>
2018-10-30 11:34:13 +0900

    Taxiクラス作成及びCarクラス、スタートプログラム修正

--- src/main/java/jp/co/rakus/ControllerSample1Application.java
+++ src/main/java/jp/co/rakus/ControllerSample1Application.java
@@ -2,11 +2,13 @@ package jp.co.rakus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.SystemPropertyUtils;
 
 @SpringBootApplication
 public class ControllerSample1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControllerSample1Application.class, args);
+		System.out.println();
 	}
 }
---
+++ src/main/java/jp/co/rakus/Taxi.java
@@ -0,0 +1,16 @@
+package jp.co.rakus;
+
+public class Taxi extends Car {
+	
+	private int price;
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+
+}
